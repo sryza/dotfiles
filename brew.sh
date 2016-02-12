674cc27c1919919ae7603917e848619daa621667
Uri Laserson <uri.laserson@gmail.com>
2016-02-11 23:41:21 -0800

    some tweaks

--- brew.sh
+++ brew.sh
@@ -49,30 +49,30 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install nmap
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
 brew install ack
@@ -121,12 +121,13 @@ brew install thrift
 brew install watch
 brew install xz
 
-brew install python
-pip install -U pip
-pip install -U setuptools
-brew install python3
-pip3 install -U pip
-pip3 install -U setuptools
+# HANDLE WITH CONDA INSTEAD
+# brew install python
+# pip install -U pip
+# pip install -U setuptools
+# brew install python3
+# pip3 install -U pip
+# pip3 install -U setuptools
 brew install ruby
 brew install r
 
